ba219c75533fcc06281678d633d4a0bd5e6ec162
DiegoLara-FlagerCraft <80766432+DiegoLara-FlagerCraft@users.noreply.github.com>
2024-10-09 18:38:00 -0500

    Delete RMI_CLUSTER directory

--- RMI_CLUSTER/src/main/java/rmi/ClientCallback.java
+++
@@ -1,8 +0,0 @@
-package rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientCallback extends Remote {
-    void onResult(String result) throws RemoteException;
-}
